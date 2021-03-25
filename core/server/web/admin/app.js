3856064dd091f849845ac23eb5b2229a013757cb
Will <will@onezerohosting.com>
2021-03-25 10:04:52 -0700

    Fixed livereload and ember only available via localhost

--- core/server/web/admin/app.js
+++ core/server/web/admin/app.js
@@ -25,7 +25,7 @@ module.exports = function setupAdminApp() {
     // Ember CLI's live-reload script
     if (config.get('env') === 'development') {
         adminApp.get('/ember-cli-live-reload.js', function emberLiveReload(req, res) {
-            res.redirect(`http://localhost:4200${urlUtils.getSubdir()}/ghost/ember-cli-live-reload.js`);
+            res.redirect(`${req.protocol}://${req.host}:4200${urlUtils.getSubdir()}/ghost/ember-cli-live-reload.js`);
         });
     }
 
